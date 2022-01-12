f826fd016053a1e5620d4e511424fd169121ad2e
DavidGL-UC3M <100363815@alumnos.uc3m.es>
2022-01-12 19:05:12 +0100

    generador

--- generador.py
+++ generador.py
@@ -1,18 +1,18 @@
-rows, columns = 4, 4
 
-outputFile = "salida.txt"
+for i in (3,6,10):
+    outputFile = f"salida{i}x{i}.txt"
 
-string = ""
-for i in range(rows):
-    for j in range(columns):
-        string += f"t{i}{j} "
+    string = ""
+    for i in range(i):
+        for j in range(i):
+            string += f"t{i}{j} "
 
-string += "- tile\n\n"
+    string += "- tile\n\n"
 
-for i in range(rows):
-    for j in range(columns):
-        string += f"(= (x t{i}{j}) {i}) (= (y t{i}{j}) {j})"
-        
-with open(outputFile, "w") as f:
+    for i in range(i):
+        for j in range(i):
+            string += f"(= (x t{i}{j}) {i}) (= (y t{i}{j}) {j})"
+            
+    with open(outputFile, "w") as f:
 
-    f.write(string)
+        f.write(string)
